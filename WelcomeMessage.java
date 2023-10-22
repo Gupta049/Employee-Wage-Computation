2162a5bdee9094421aed1f102084d4b4eb328c87
“Gupta049” <sushilgupta049@gmail.com>
2023-10-22 10:33:09 +0530

    [Sushil] - Welcome to our Employee Wagw Problem

---
+++ WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeewagecalculation;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("WelCome to our Employee Wage Computation Problem. ");
+    }
+}
